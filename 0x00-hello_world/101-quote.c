1dc1a1bf4daf365af052e5b0aef676cfbb6f0f63
spencerjmk <olajumokebakare76@gmail.com>
2023-07-16 20:14:01 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,26 @@
+#include <wchar.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+	char * str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int str_len = strlen ( str );
+	int cnt = 0;
+
+	while ( 1 )
+	{
+		if ( cnt == str_len )
+		{
+			break;
+		}
+		else
+		{
+			fputwc(str[cnt], stderr);
+		}
+
+		cnt ++;
+	}
+	
+	return 1;
+}
