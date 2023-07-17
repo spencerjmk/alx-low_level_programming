01f1789321e59e8dead8f9994c055e1a9ea08455
spencerjmk <olajumokebakare76@gmail.com>
2023-07-16 20:37:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,20 @@
 #include <wchar.h>
 #include <stdio.h>
 #include <string.h>
-
-int main (void)
+/**
+ * main - Entry point
+ * description: writes to file
+ * @void: does nothing
+ * Return: Always 1 (success)
+ */
+int main(void)
 {
-	char * str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	int str_len = strlen ( str );
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int str_len = strlen(str);
 	int cnt = 0;
-
-	while ( 1 )
+	while (1)
 	{
-		if ( cnt == str_len )
+		if (cnt == str_len)
 		{
 			break;
 		}
@@ -18,9 +22,7 @@ int main (void)
 		{
 			fputwc(str[cnt], stderr);
 		}
-
-		cnt ++;
+		cnt++;
 	}
-	
-	return 1;
+	return (1);
 }
