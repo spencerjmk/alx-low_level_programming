5c3893862d7d1932d14408286782acea2248b51b
spencerjmk <olajumokebakare76@gmail.com>
2023-07-20 20:55:02 -0700

    0-main.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 /**
  * _isupper - check uppercase char
  * @c: the argument
- *
  * Return: 1 for uppercase letter 0 for others
  */
-int _isupper (int c)
+int _isupper(int c)
 {
 	int a = 0;
-	if ( c >= 65 && c <= 90)
+
+	if (c >= 65 && c <= 90)
 	{
 		a = 1;
 	}
-	return a;
+	return (a);
 }
 
