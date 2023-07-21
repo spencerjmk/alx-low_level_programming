f0c2069a3cf9c08ce78ca2f2cc0aca2375d72b79
spencerjmk <olajumokebakare76@gmail.com>
2023-07-20 21:04:51 -0700

    1-main.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+/**
+ * _isupper - check uppercase char
+ * @c: the argument
+ * Return: 1 for uppercase letter 0 for others
+ */
+int _isdigit(int c)
+{
+	int a = 0;
+
+	if (c >= 48 && c <= 57 )
+	{
+		a = 1;
+	}
+	return (a);
+}
+
